39ed45fcce975d7f9bcc0951848abca426fa2d43
Carol <galvao.carolina@hotmail.com>
2020-03-28 14:49:22 -0300

    terminei a aula 2

--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const OngController = require ('./controllers/OngController');
 const IncidentController = require ('./controllers/IncidentController');
+const ProfileController = require ('./controllers/ProfileController');
+const SessionController = require ('./controllers/SessionController');
 
 const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', OngController.listOng);
 routes.post('/ongs', OngController.create);
 
 routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.listIncident);
+routes.delete('/incidents/:id', IncidentController.deleteIncidentById);
+
+routes.get('/profile', ProfileController.list);
 
 module.exports = routes;
